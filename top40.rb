8327802d8287b790966cf55957187477421fbbf1
Kevin Ndung'u <kevgathuku@gmail.com>
2015-01-02 15:44:22 +0300

    First working version with Cache

---
+++ top40.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+require 'json'
+require 'net/http'
+require 'tmpdir'
+require 'youtube_search'
+
+# Source: https://developer.yahoo.com/ruby/ruby-cache.html
+class DiskFetcher
+  def initialize(
+    url='http://ben-major.co.uk/labs/top40/api/singles/',
+    cache_dir=Dir.tmpdir)
+      # this is the dir where we store our cache
+      @cache_dir = cache_dir
+      @url = url
+  end
+
+  def fetch(max_age = 43200)
+      file = Digest::MD5.hexdigest(@url)
+      file_path = File.join("", @cache_dir, file)
+      # we check if the file -- a MD5 hexdigest of the URL -- exists
+      #  in the dir. If it does and the data is fresh, we just read
+      #  data from the file and return
+      if File.exist? file_path
+        return File.new(file_path).read if Time.now - File.mtime(file_path) < max_age
+      end
+      # if the file does not exist (or if the data is not fresh), we
+      #  make an HTTP request and save it to a file
+      File.open(file_path, 'w') do |data|
+        res = Net::HTTP.get_response(URI.parse(@url))
+        data << res.body if res.code == '200'
+      end
+  end
+
+  def display
+    file_name = Digest::MD5.hexdigest(@url)
+    json_file = File.join("", @cache_dir, file_name)
+    content = JSON.load(File.read(json_file, :encoding => 'utf-8'))
+    content['entries'].each do |entry|
+      puts "#{entry['title']} by #{entry['artist']}"
+    end
+  end
+end
+
+fetcher = DiskFetcher.new
+fetcher.fetch
+fetcher.display()
+# if 'links' in ARGV
+#   search = YoutubeSearch.search(content['entries'][0]['title']).first
+#   puts "#{content['entries'][0]['title']} -
+# http://youtu.be/#{search['video_id']}"
