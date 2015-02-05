2a3f8a6903fa2543915b920735b7e53445a3a138
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-06 01:44:41 +0300

    Initial implementation of ChartInfo class

--- yamlstore.rb
+++ yamlstore.rb
@@ -1,12 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'dotenv'
+require 'lastfm'
 require 'yaml/store'
+require 'youtube_search'
+require File.join(File.dirname(__FILE__), 'top40')
+Dotenv.load
 
-store = YAML::Store.new "top40singles.yaml"
-Single = Struct.new :title, :artist, :id
+Single = Struct.new :title, :artist, :mbid, :lastfm_id, :youtube
 
-store.transaction do
-  store['top40'] ||= []
-  store['top40'].push Single.new 'Uptown Funk (feat. Bruno Mars)',
-    'Mark Ronson', 'OPf0YbXqDm0'
-  store['top40'].push Single.new 'Take Me To Church', 'Hozier',
-    'MYSVMgRr6pw'
+# Repsonsible for storing info related to the Top 40 Singles
+class ChartInfo
+  def initialize
+    @charts = Top40.new
+    @store = YAML::Store.new 'top40singles.yaml'
+    @lastfm = Lastfm.new(ENV['LASTFM_API_KEY'], ENV['LASTFM_API_SECRET'])
+  end
+
+  def get_ids(artist, track)
+    track = @lastfm.track.get_info(artist: artist, track: track)
+    [track['mbid'], track['id']]
+  end
+
+  def get_youtube(artist, track)
+    link = YoutubeSearch.search("#{artist} - #{track}").first
+    "https://youtu.be/#{link['video_id']}"
+  end
+
+  def store
+    store.transaction do
+      store['top40'] ||= []
+      @charts.singles.each do |song|
+        store['top40'].push Single.new(
+          "#{song['title']}",
+          "#{song['artist']}",
+          get_ids("#{song['title']}", "#{song['artist']}"),
+          get_youtube("#{song['title']}", "#{song['artist']}")
+          )
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  singles_info = ChartInfo.new
+  singles_info.store
 end
