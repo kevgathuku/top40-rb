f5e0a2b3c04d1e797b49f659269dd656966e4792
Kevin Ndung'u <kevgathuku@gmail.com>
2015-02-08 23:33:00 +0300

    Use APICache to cache youtube results

--- yamlstore.rb
+++ yamlstore.rb
@@ -2,34 +2,25 @@
 
 # require 'dotenv'
 # require 'lastfm'
+require 'api_cache'
 require 'yaml/store'
 require 'youtube_search'
 require File.join(File.dirname(__FILE__), 'top40')
 # Dotenv.load
 
-Single = Struct.new :title, :artist, :youtube
-
 # Repsonsible for storing info related to the Top 40 Singles
 class ChartInfo
   def initialize
     @charts = Top40.new
-    @store = YAML::Store.new 'top40singles.yaml'
   end
 
   def get_youtube(artist, track)
-    cached = @store.transaction(true) do
-      @store.abort if @store.root?('top40') == false
-      @store['top40'].select {
-        |song| song.artist == artist && song.title == title
-      }.first
+    cached = APICache.get('youtube link', fail: ['Read failed']) do
+      link = YoutubeSearch.search("#{artist} - #{track}").first
+      "https://youtu.be/#{link['video_id']}"
     end
-    return cached.youtube unless cached.youtube.empty?
-  rescue NoMethodError => e
-    puts e
-    link = YoutubeSearch.search("#{artist} - #{track}").first
-    "https://youtu.be/#{link['video_id']}"
+    cached
   end
-
 
   def populate_objects
     @charts.singles.each do |song|
@@ -37,16 +28,11 @@ class ChartInfo
     end
   end
 
-  def save
-    @store.transaction do
-      @store['top40'] ||= []
-      @charts.singles.each do |song|
-        @store['top40'].push Single.new(
-          "#{song['title']}",
-          "#{song['artist']}",
-          "#{song['youtube']}"
-          )
-      end
+  def display
+    @charts.singles.each do |entry|
+      output = "#{entry['position']}. #{entry['artist']} - #{entry['title']}"
+      output << " (#{entry['youtube']})"
+      puts output
     end
   end
 end
@@ -54,5 +40,5 @@ end
 if __FILE__ == $PROGRAM_NAME
   singles_info = ChartInfo.new
   singles_info.populate_objects
-  singles_info.save
+  singles_info.display
 end
